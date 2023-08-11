959a53b0b0b0a07e88305fde04ac9e4a30fe9cfc
Senthuran Selvanathan <46525254+sselvanathan@users.noreply.github.com>
2023-08-11 18:53:09 +0200

    Fixed a minor bug where size values could not be searched

--- app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
@@ -34,7 +34,7 @@ export const SizeClient: React.FC<SizeClientProps> = (
                 </Button>
             </div>
             <Separator/>
-            <DataTable columns={columns} data={data} searchKey="label" />
+            <DataTable columns={columns} data={data} searchKey="name" />
             <Heading title="API" description="API calls for Sizes" />
             <Separator />
             <ApiList entityName="sizes" entityIdName="sizeId"/>
