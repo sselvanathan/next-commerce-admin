3034fe2e722a1f98272c763eb846a47a930a8ef9
Senthuran Selvanathan <46525254+sselvanathan@users.noreply.github.com>
2023-08-25 20:26:45 +0200

    fix login positioning

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,7 +8,7 @@ export default function AuthLayout(
     }
 ){
     return (
-        <div className="flex item-center justify-center h-full">
+        <div className="flex items-center justify-center h-full w-full">
             {children}
         </div>
     )
